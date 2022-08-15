a3225d65675b895e568ad63fdf2dc33bb3101e6e
Wyatt Gassaway (Credera) <wyatt.gassaway@credera.com>
2022-08-15 11:39:23 -0500

    Add Error checking to fizzbuzz.py

--- back-end/fizzbuzz.py
+++ back-end/fizzbuzz.py
@@ -3,14 +3,16 @@
 #Both = fizzbuzz
 value = input("This program will evalue numbers from 1 to x. What you would like x to be? Please only put in a numerical value. \n")
 
-print("\n")
-va1ue = int(value) + 1
-for x in range(1, va1ue):
-    if x % 3 == 0 and x % 5 == 0:
-        print("FizzBuzz!")
-    elif x % 3 == 0:
-        print("Fizz")
-    elif x % 5 == 0:
-        print("Buzz")
-    else:
-        print(x)
+if not value.isnumeric():
+    print("That's not a numerical value!")
+else:
+    print("")
+    for x in range(1, value + 1):
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz!")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
